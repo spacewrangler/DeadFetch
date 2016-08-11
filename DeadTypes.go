2350410a73b4619bc02bcbfde1c208d5f4ce60c2
Jon Langdon <jon.p.langdon@gmail.com>
2016-08-11 00:12:46 -0700

    added a logger and more JSON unmarshaling changes

--- DeadTypes.go
+++ DeadTypes.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 )
 
 // Params contains the query parameters passed to archive.org
-type ArchiveSearchQueryParams struct {
+type archiveSearchQueryParams struct {
 	Query   string `json:"q"`
 	QueryIn string `json:"qin"`
 	Rows    int    `json:",string"`
 	Start   int
 }
 
-type ArchiveSearchResponseHeader struct {
+type archiveSearchResponseHeader struct {
 	Status int
 	QTime  int
-	Params ArchiveSearchQueryParams
+	Params archiveSearchQueryParams
 }
 
 type ArchiveDoc struct {
@@ -23,23 +27,86 @@ type ArchiveDoc struct {
 	OaiUpdateDate []time.Time `json:"oai_updatedate"`
 }
 
-type ArchiveSearchResults struct {
+type archiveSearchResults struct {
 	NumFound int
 	Start    int
 	Docs     []ArchiveDoc
 }
 
-type ArchiveSearchResponse struct {
-	ResponseHeader ArchiveSearchResponseHeader
-	Response       ArchiveSearchResults
+type archiveSearchResponse struct {
+	ResponseHeader archiveSearchResponseHeader
+	Response       archiveSearchResults
+}
+
+type deadShowRaw struct {
+	Server   string
+	Metadata deadShowMetadataRaw `json:"metadata"`
 }
 
 type DeadShow struct {
-	Server   string
-	Metadata DeadShowMetadata `json:"metadata"`
+	Server         string
+	Identifier     []string
+	Title          []string
+	Creator        []string
+	Mediatype      []string
+	Collection     []string
+	Type           []string
+	Description    []string
+	Date           []string
+	Year           []string
+	Subject        []string
+	PublicDate     []string
+	AddedDate      []string
+	Venue          []string
+	Coverage       []string
+	Source         []string
+	Lineage        []string
+	Taper          []string
+	Transferer     []string
+	RunTime        []string
+	md5s           []string
+	Notes          []string
+	UpdateDate     []string
+	Updater        []string
+	Curation       []string
+	BackupLocation []string
 }
 
-type DeadShowMetadata struct {
+func (ds *DeadShow) UnmarshalJSON(data []byte) error {
+	var dsr deadShowRaw
+	json.Unmarshal(data, &dsr)
+
+	ds.Server = dsr.Server
+	ds.Identifier = dsr.Metadata.Identifier
+	ds.Title = dsr.Metadata.Title
+	ds.Creator = dsr.Metadata.Creator
+	ds.Mediatype = dsr.Metadata.Mediatype
+	ds.Collection = dsr.Metadata.Collection
+	ds.Type = dsr.Metadata.Type
+	ds.Description = dsr.Metadata.Description
+	ds.Date = dsr.Metadata.Date
+	ds.Year = dsr.Metadata.Year
+	ds.Subject = dsr.Metadata.Year
+	ds.PublicDate = dsr.Metadata.PublicDate
+	ds.AddedDate = dsr.Metadata.AddedDate
+	ds.Venue = dsr.Metadata.Venue
+	ds.Coverage = dsr.Metadata.Coverage
+	ds.Source = dsr.Metadata.Source
+	ds.Lineage = dsr.Metadata.Lineage
+	ds.Taper = dsr.Metadata.Taper
+	ds.Transferer = dsr.Metadata.Transferer
+	ds.RunTime = dsr.Metadata.RunTime
+	ds.md5s = dsr.Metadata.md5s
+	ds.Notes = dsr.Metadata.Notes
+	ds.UpdateDate = dsr.Metadata.UpdateDate
+	ds.Updater = dsr.Metadata.Updater
+	ds.Curation = dsr.Metadata.Curation
+	ds.BackupLocation = dsr.Metadata.BackupLocation
+
+	return nil
+}
+
+type deadShowMetadataRaw struct {
 	Identifier     []string
 	Title          []string
 	Creator        []string
@@ -67,11 +134,71 @@ type DeadShowMetadata struct {
 	BackupLocation []string `json:"backup_location"`
 }
 
-type DeadShowFiles struct {
-	Files map[string]DeadShowFile
+type deadShowFilesRaw struct {
+	Files map[string]deadShowFileRaw
+}
+
+type deadShowFileRaw struct {
+	Name   string
+	Source string
+	Format string
 }
 
 type DeadShowFile struct {
+	// TODO add show ID
+	//ShowIdentifier string
+	Name   string
 	Source string
 	Format string
+}
+
+type DeadShowFiles struct {
+	Files []DeadShowFile
+}
+
+func (dsf *DeadShowFiles) UnmarshalJSON(data []byte) error {
+	// These var names are confusing: too many "file"
+	var deadFiles deadShowFilesRaw
+	var deadFile DeadShowFile
+	json.Unmarshal(data, &deadFiles)
+	for k, v := range deadFiles.Files {
+		deadFile.Name = strings.TrimPrefix(k, "/")
+		deadFile.Source = v.Source
+		deadFile.Format = v.Format
+		dsf.Files = append(dsf.Files, deadFile)
+	}
+
+	return nil
+}
+
+func searchDeadShows(numberOfResults int, startPage int) []ArchiveDoc {
+
+	var searchURL = fmt.Sprint("https://archive.org/advancedsearch.php?"+
+		"q=collection%3AGratefulDead&fl%5B%5D=identifier&sort%5B%5D=identifier+"+
+		"desc&rows=", numberOfResults, "&page=", startPage, "&output=json")
+
+	// Print search URL to the console
+	fmt.Println(searchURL)
+
+	r, _ := http.Get(searchURL)
+	defer r.Body.Close()
+
+	var searchResponse archiveSearchResponse
+	// Should this be a look until EOF? Or am I sure I have all the response?
+	// Is that what the Body.Close call gets me?
+	json.NewDecoder(r.Body).Decode(&searchResponse)
+
+	// Print search result details to the console
+	fmt.Println("Rows found: ", searchResponse.Response.NumFound)
+	fmt.Println("Rows returned: ", searchResponse.ResponseHeader.Params.Rows)
+	fmt.Println("Starting record: ", searchResponse.Response.Start)
+
+	var docs = []ArchiveDoc{}
+
+	for _, doc := range searchResponse.Response.Docs {
+		fmt.Println(doc.Identifier)
+		docs = append(docs, doc)
+	}
+
+	return docs
 }
--- main.go
+++ main.go
@@ -5,45 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"time"
 	//"io/ioutil" // Needed for the json.unmarshal usage below
 	"net/http"
 )
 
-func searchDeadShows(numberOfResults int, startPage int) []ArchiveDoc {
-
-	var searchURL = fmt.Sprint("https://archive.org/advancedsearch.php?"+
-		"q=collection%3AGratefulDead&fl%5B%5D=identifier&sort%5B%5D=identifier+"+
-		"desc&rows=", numberOfResults, "&page=", startPage, "&output=json")
-
-	// Print search URL to the console
-	fmt.Println(searchURL)
-
-	r, _ := http.Get(searchURL)
-	defer r.Body.Close()
-
-	var searchResponse ArchiveSearchResponse
-	// Should this be a look until EOF? Or am I sure I have all the response?
-	// Is that what the Body.Close call gets me?
-	json.NewDecoder(r.Body).Decode(&searchResponse)
-
-	// Print search result details to the console
-	fmt.Println("Rows found: ", searchResponse.Response.NumFound)
-	fmt.Println("Rows returned: ", searchResponse.ResponseHeader.Params.Rows)
-	fmt.Println("Starting record: ", searchResponse.Response.Start)
-
-	var docs = []ArchiveDoc{}
-
-	for _, doc := range searchResponse.Response.Docs {
-		fmt.Println(doc.Identifier)
-		docs = append(docs, doc)
-	}
-
-	return docs
-}
-
 // TODO: NO error handling. Should probably do something about that.
 func main() {
+	LogInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
 
 	numResults := flag.Int("numResults", 50, "Number of results to return")
 	startPage := flag.Int("startPage", 0, "Results page to start with")
@@ -57,7 +27,7 @@ func main() {
 	for _, doc := range results {
 		fmt.Println(doc.Identifier)
 		showURL := "http://archive.org/details/" + doc.Identifier + "?output=json"
-		fmt.Println(showURL)
+		Trace.Println(showURL)
 		r, _ := http.Get(showURL)
 		defer r.Body.Close()
 
@@ -68,27 +38,22 @@ func main() {
 		// Is that what the Body.Close call gets me?
 		//json.NewDecoder(r.Body).Decode(&showResponse)
 
-		// Alternative solution using Unmarshal
-		// show, _ := ioutil.ReadAll(s.Body)
 		json.Unmarshal(showJSON, &showResponse)
-
-		var deadFiles DeadShowFiles
-		json.Unmarshal(showJSON, &deadFiles)
-		for k, v := range deadFiles.Files {
-			fmt.Println(k)
-			fmt.Println(v)
-		}
 
 		// :( All these will panic if value is nil
 		fmt.Println("Server: ", showResponse.Server)
-		fmt.Println("Date: ", showResponse.Metadata.Date[0])
-		t, _ := time.Parse("2006-01-02", showResponse.Metadata.Date[0])
+		fmt.Println("Date: ", showResponse.Date[0])
+		t, _ := time.Parse("2006-01-02", showResponse.Date[0])
 		fmt.Println("Parsed Date: ", t.Format("2006-01-02"))
-		fmt.Println("Venue: ", showResponse.Metadata.Venue[0])
-		fmt.Println("Backup Location: ", showResponse.Metadata.BackupLocation[0])
+		fmt.Println("Venue: ", showResponse.Venue[0])
+		fmt.Println("Backup Location: ", showResponse.BackupLocation[0])
 
 		// Dump the whole showResponse to console
 		//fmt.Printf("%+v\n", showResponse)
+
+		var showFiles DeadShowFiles
+		json.Unmarshal(showJSON, &showFiles)
+		fmt.Println(showFiles.Files)
 
 		fmt.Scanln()
 	}
