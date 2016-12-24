7671281aa89dc6f1f9677c64326160f768a40042
Jon Langdon <jon.p.langdon@gmail.com>
2016-12-24 12:24:24 -0800

    Adding command-line flags for ES refactoring

--- main.go
+++ main.go
@@ -13,20 +13,45 @@ import (
 	"gopkg.in/olivere/elastic.v5"
 )
 
-// TODO: NO error handling. Should probably do something about that.
-func main() {
-	LogInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+type Config struct {
+	GeoLookup            bool
+	IndexToElasticSearch bool
+	ElasticsearchURL     string
+	NumberOfResults      int
+	StartPage            int
+}
 
-	numResults := flag.Int("numResults", 50, "Number of results to return")
-	startPage := flag.Int("startPage", 0, "Results page to start with")
-	flag.Parse()
+var config *Config
+var client *elastic.Client
 
-	fmt.Println("numResults: ", *numResults)
-	fmt.Println("startPage: ", *startPage)
+func init() {
+	const (
+		elasticsearchURLDefault = ""
+		elasticsearchURLDesc    = "Elasticsearch URL to index Dead shows"
+		numberOfResultsDefault  = 50
+		numberOfResultsDesc     = "Number of results to return from Archive search"
+		startPageDefault        = 0
+		startPageDesc           = "Search result page to start with"
+		geoLookupDefault        = false
+		geoLookupDesc           = "Lookup lat/long coordinates for shows"
+	)
 
+	config = &Config{}
+
+	flag.IntVar(&config.NumberOfResults, "numresults", numberOfResultsDefault, numberOfResultsDesc)
+	flag.IntVar(&config.StartPage, "startpage", startPageDefault, startPageDesc)
+	flag.StringVar(&config.ElasticsearchURL, "es-url", elasticsearchURLDefault, elasticsearchURLDesc)
+	flag.BoolVar(&config.GeoLookup, "geolookup", geoLookupDefault, geoLookupDesc)
+}
+
+func initElasticsearch(url *string) {
 	// Setup elastic
 	errorlog := log.New(os.Stdout, "APP ", log.LstdFlags)
-	client, err := elastic.NewClient(elastic.SetErrorLog(errorlog))
+
+	var err error
+	client, err = elastic.NewClient(
+		elastic.SetErrorLog(errorlog),
+		elastic.SetURL(*url))
 	if err != nil {
 		// Handle error
 		panic(err)
@@ -35,11 +60,12 @@ func main() {
 	// Trace request and response details like this
 	//client.SetTracer(log.New(os.Stdout, "", 0))
 	// Ping the Elasticsearch server to get e.g. the version number
-	info, code, err := client.Ping("http://127.0.0.1:9200").Do(context.TODO())
+	info, code, err := client.Ping(config.ElasticsearchURL).Do(context.TODO())
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
+
 	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 
 	exists, err := client.IndexExists("deadshows").Do(context.TODO())
@@ -59,49 +85,82 @@ func main() {
 			// Not acknowledged
 		}
 	}
+}
+
+func indexDeadShow(show *DeadShow) {
+	Trace.Println("Indexing:", *show.Identifier)
+
+	details, _ := json.Marshal(show.Details)
+	put1, err := client.Index().
+		Index("deadshows").
+		Type("deadshow").
+		Id(string(*show.Identifier)).
+		// TODO: this has been null before. Check that show is actually retrieved.
+		// If not, re-fetch
+		BodyString(string(details)).
+		Do(context.TODO())
+	if err != nil {
+		// Handle error
+		panic(err)
+	}
+
+	Trace.Println("Created:", put1.Created, "Version:", put1.Version)
+}
+
+func fetchDeadShow(id string) DeadShow {
+	showURL := "http://archive.org/details/" + id + "?output=json"
+	Trace.Println(showURL)
+	r, _ := http.Get(showURL)
+	defer r.Body.Close()
+
+	showJSON, _ := ioutil.ReadAll(r.Body)
+
+	var showResponse DeadShow
+	json.Unmarshal(showJSON, &showResponse)
+
+	if showResponse.Details.Coverage != nil {
+		fmt.Println(*showResponse.Details.Coverage)
+	} else {
+		fmt.Println("No location given for show")
+	}
+	if showResponse.Details.LatLong != nil {
+		fmt.Println(*showResponse.Details.LatLong)
+	} else {
+		fmt.Println("No Lat/Long available for show")
+	}
+
+	return showResponse
+}
+
+func main() {
+	LogInit(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+
+	flag.Parse()
+	if config.ElasticsearchURL == "" {
+		config.IndexToElasticSearch = false
+	} else {
+		config.IndexToElasticSearch = true
+	}
+
+	fmt.Println("numResults:", config.NumberOfResults)
+	fmt.Println("startPage:", config.StartPage)
+	fmt.Println("elasticsearch URL:", config.ElasticsearchURL)
+	fmt.Println("geolookup:", config.GeoLookup)
+	fmt.Println("index to ES:", config.IndexToElasticSearch)
+
+	initElasticsearch(&config.ElasticsearchURL)
 
 	// Print show details to the console
-	results := SearchDeadShows(*numResults, *startPage)
-	Trace.Println("Number of results: {0}", len(results))
+	results := SearchDeadShows(config.NumberOfResults, config.StartPage)
+	Trace.Println("Number of results:", len(results))
+	var show DeadShow
 	for _, doc := range results {
-		showURL := "http://archive.org/details/" + doc.Identifier + "?output=json"
-		Trace.Println(showURL)
-		r, _ := http.Get(showURL)
-		defer r.Body.Close()
+		show = fetchDeadShow(doc.Identifier)
 
-		showJSON, _ := ioutil.ReadAll(r.Body)
+		if config.IndexToElasticSearch {
+			indexDeadShow(&show)
 
-		var showResponse DeadShow
-		json.Unmarshal(showJSON, &showResponse)
-
-		show, _ := json.Marshal(showResponse.Details)
-		if showResponse.Details.Coverage != nil {
-			fmt.Println(*showResponse.Details.Coverage)
-		} else {
-			fmt.Println("No location given for show")
 		}
-		if showResponse.Details.LatLong != nil {
-			fmt.Println(*showResponse.Details.LatLong)
-		} else {
-			fmt.Println("No Lat/Long available for show")
-		}
-
-		Trace.Println("Indexing: {0}", *showResponse.Identifier)
-		put1, err := client.Index().
-			Index("deadshows").
-			Type("deadshow").
-			Id(string(*showResponse.Identifier)).
-			// TODO: this has been null before. Check that show is actually retrieved.
-			// If not, re-fetch
-			BodyString(string(show)).
-			Do(context.TODO())
-		if err != nil {
-			// Handle error
-			panic(err)
-		}
-
-		Trace.Println("Created:", put1.Created, "Version:", put1.Version)
-
 		// get1, err := client.Get().
 		// 	Index("deadshows").
 		// 	Type("deadshow").
